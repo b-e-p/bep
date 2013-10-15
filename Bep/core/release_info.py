15dfb521affe2d73c6b9605d30e827ab13e9330a
Jason Gors <jason.gors.work@gmail.com>
2013-10-15 11:05:21 -0400

    updated release_info

--- Bep/core/release_info.py
+++ Bep/core/release_info.py
@@ -10,7 +10,7 @@ Purpose:  general info that gets put into setup.py and shared elsewhere.
 
 name = 'bep'
 __version__ = '0.5.1dev'
-description = 'A tool to make Package installation easier.'
+description = 'A tool to make package installation easier.'
 author = 'jason gors'
 email = 'jasonDOTgorsATgmailDOTcom'
 url = 'http://www.github.com/bepy/bep.git'
