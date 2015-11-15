c47fac5154ddf598746c60f42b3eb1d802eb611d
Jason Gors <jason.gors.work@gmail.com>
2015-11-15 14:09:14 -0500

    update to unit-tests

--- Bep/tests/test_commands.py
+++ Bep/tests/test_commands.py
@@ -3,8 +3,30 @@
 #----------------------------------------------------------------
 # Author: Jason Gors <jasonDOTgorsATgmail>
 # Creation Date: 09-20-2013
-# Purpose: to make sure the commands passed in work as expected.
+# Purpose: to make sure that everything works as expected.
 #----------------------------------------------------------------
 
-# TODO
-#from Bep import package
+import subprocess
+
+
+def test_install():
+    ret_code = subprocess.call("bep install github pypa/pip", shell=True)
+    assert ret_code == 0
+
+def test_update():
+    ret_code = subprocess.call("bep update github pip --branch=master", shell=True)
+    assert ret_code == 0
+
+def test_turn_off():
+    ret_code = subprocess.call("bep turn_off github pip --branch=master", shell=True)
+    assert ret_code == 0
+
+def test_turn_on():
+    ret_code = subprocess.call("bep turn_on github pip --branch=master", shell=True)
+    assert ret_code == 0
+
+def test_remove():
+    ret_code = subprocess.call("bep remove github pip --branch=master", shell=True)
+    assert ret_code == 0
+
+
