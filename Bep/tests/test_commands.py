c753abc1540c490c045f42a5c0f898e1a9b39328
Jason Gors <jason.gors.work@gmail.com>
2015-11-21 19:30:24 -0500

    remove locally embedded repos and revert to unit-tests

--- Bep/tests/test_commands.py
+++ Bep/tests/test_commands.py
@@ -24,51 +24,51 @@ bitbucket_repo_hg = 'jgors/testrepo_hg'
 repo_name = lambda r: r.split('/')[-1]
 
 
-# def check_local_repo(cmd, local_repo_name, branch):
-    # try:
-        # tdir = tempfile.mkdtemp()
-        # local_repo_dir = os.path.join(tdir, local_repo_name)
+def check_local_repo(cmd, local_repo_name, branch):
+    try:
+        tdir = tempfile.mkdtemp()
+        local_repo_dir = os.path.join(tdir, local_repo_name)
 
-        # # cmd = "git clone https://github.com/b-e-p/testrepo1 {}".format(local_repo_dir)
-        # sp_cmd = "{} {}".format(cmd, local_repo_dir)
-        # ret_code = subprocess.call(sp_cmd, shell=True)
-        # ok_(ret_code == 0)
+        # cmd = "git clone https://github.com/b-e-p/testrepo1 {}".format(local_repo_dir)
+        sp_cmd = "{} {}".format(cmd, local_repo_dir)
+        ret_code = subprocess.call(sp_cmd, shell=True)
+        ok_(ret_code == 0)
 
-        # ret_code = subprocess.call("bep install local {}".format(local_repo_dir), shell=True)
-        # ok_(ret_code == 0)
+        ret_code = subprocess.call("bep install local {}".format(local_repo_dir), shell=True)
+        ok_(ret_code == 0)
 
-        # ret_code = subprocess.call("bep turn_off local {} --branch={}".format(local_repo_name, branch), shell=True)
-        # ok_(ret_code == 0)
+        ret_code = subprocess.call("bep turn_off local {} --branch={}".format(local_repo_name, branch), shell=True)
+        ok_(ret_code == 0)
 
-        # ret_code = subprocess.call("bep turn_on local {} --branch={}".format(local_repo_name, branch), shell=True)
-        # ok_(ret_code == 0)
+        ret_code = subprocess.call("bep turn_on local {} --branch={}".format(local_repo_name, branch), shell=True)
+        ok_(ret_code == 0)
 
-        # ret_code = subprocess.call("bep update local {} --branch={}".format(local_repo_name, branch), shell=True)
-        # ok_(ret_code == 0)
+        ret_code = subprocess.call("bep update local {} --branch={}".format(local_repo_name, branch), shell=True)
+        ok_(ret_code == 0)
 
-        # ret_code = subprocess.call("bep remove local {} --branch={}".format(local_repo_name, branch), shell=True)
-        # ok_(ret_code == 0)
-    # finally:
-        # shutil.rmtree(tdir)
+        ret_code = subprocess.call("bep remove local {} --branch={}".format(local_repo_name, branch), shell=True)
+        ok_(ret_code == 0)
+    finally:
+        shutil.rmtree(tdir)
 
-
-# def test_local_repo():
-    # yield check_local_repo, 'git clone https://github.com/{}'.format(github_repo1), 'github_testrepo_local1', 'master'
-    # yield check_local_repo, 'git clone https://github.com/{}'.format(github_repo2), 'github_testrepo_local2', 'master'
-    # yield check_local_repo, 'git clone https://bitbucket.org/{}'.format(bitbucket_repo_git), 'bitbucket_testrepo_git_local', 'master'
-    # yield check_local_repo, 'hg clone https://bitbucket.org/{}'.format(bitbucket_repo_hg), 'bitbucket_testrepo_hg_local', 'default'
-
-def check_local_repo(local_repo_name, branch_name):
-    testrepo_local = os.path.join(pwd, 'testrepos_local', local_repo_name)
-    ret_code = subprocess.call("bep install local {}".format(testrepo_local), shell=True)
-    ok_(ret_code == 0)
-    ret_code = subprocess.call("bep remove local {} --branch={}".format(local_repo_name, branch_name), shell=True)
-    ok_(ret_code == 0)
 
 def test_local_repo():
-    yield check_local_repo, 'testrepo_local_git', 'master'
-    yield check_local_repo, 'testrepo_local_hg', 'default'
+    yield check_local_repo, 'git clone https://github.com/{}'.format(github_repo1), 'github_testrepo_local1', 'master'
+    yield check_local_repo, 'git clone https://github.com/{}'.format(github_repo2), 'github_testrepo_local2', 'master'
+    yield check_local_repo, 'git clone https://bitbucket.org/{}'.format(bitbucket_repo_git), 'bitbucket_testrepo_git_local', 'master'
+    yield check_local_repo, 'hg clone https://bitbucket.org/{}'.format(bitbucket_repo_hg), 'bitbucket_testrepo_hg_local', 'default'
 
+# FIXME to use embedded repos for local repo cmds testing
+# def check_local_repo(local_repo_name, branch_name):
+    # testrepo_local = os.path.join(pwd, 'testrepos_local', local_repo_name)
+    # ret_code = subprocess.call("bep install local {}".format(testrepo_local), shell=True)
+    # ok_(ret_code == 0)
+    # ret_code = subprocess.call("bep remove local {} --branch={}".format(local_repo_name, branch_name), shell=True)
+    # ok_(ret_code == 0)
+
+# def test_local_repo():
+    # yield check_local_repo, 'testrepo_local_git', 'master'
+    # yield check_local_repo, 'testrepo_local_hg', 'default'
 
 
 def check_install_github_repo(github_repo):
@@ -107,9 +107,10 @@ def test_update_github_repo1():
     ret_code = subprocess.call("bep update github {} --branch=master".format(repo_name(github_repo1)), shell=True)
     ok_(ret_code == 0)
 
-def test_update_all():
-    ret_code = subprocess.call("bep update --all", shell=True)
-    ok_(ret_code == 0)
+# don't update all in unit-tests; leave potentially already installed pkgs alone.
+# def test_update_all():
+    # ret_code = subprocess.call("bep update --all", shell=True)
+    # ok_(ret_code == 0)
 
 
 def test_list():
@@ -125,9 +126,10 @@ def test_remove_bitbucket_repo_hg():
     ret_code = subprocess.call("bep remove bitbucket {} --branch=default".format(repo_name(bitbucket_repo_hg)), shell=True)
     ok_(ret_code == 0)
 
-def test_turn_off_all():
-    ret_code = subprocess.call("bep turn_off --all", shell=True)
-    ok_(ret_code == 0)
+# don't turn_off all in unit-tests; leave potentially already installed pkgs alone.
+# def test_turn_off_all():
+    # ret_code = subprocess.call("bep turn_off --all", shell=True)
+    # ok_(ret_code == 0)
 
 def test_turn_on_github_repo2():
     ret_code = subprocess.call("bep turn_off github {} --branch=master".format(repo_name(github_repo2)), shell=True)
